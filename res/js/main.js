cac6548007f0eb265a0fd49cbf2beb08acb34f45
Filip M <filipmasek1202@seznam.cz>
2023-12-05 15:28:38 +0100

    hmm

--- res/js/main.js
+++ res/js/main.js
@@ -1,4 +1,23 @@
-const canvas = document.getElementById("canvas");
+/*
+const zombie = {
+    name: "Tomas",
+    hp: 100,
+    dmg: 6,
+    drop: 15
+}
+console.log(zombie.dmg); // 6
+zombie.hp -= 50;
+*/
+
+/*
+// [] - pole
+// index - poradove cislo prvku, pocita od 0
+const foo = [5,6]
+*/
+
+
+
+/*const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const background = new Image();
 background.src = "./res/img/background.png"
@@ -43,5 +62,94 @@ window.onload = () => {
     },100);
     
 }
+*/
+
+
+
+const canvas = document.getElementById("canvas");
+const ctx = document.getElementById("2d");
+
+const background = new Image();
+background.src = "./res/img/background.png";
+
+// promenna - vymezena misto v pameti
+// {} - objekt - promenna, ktera ma ruzne vlastnosti
+// [] - pole - skupina vicero hodnot - soubor vlastnosti
+
+//promenna do ukladani vstupu z klavesnice
+// Space: true
+// Space: false
+const keys = {};
+
+//funkce ktera posloucha na nejakou akci (event)
+document.addEventListener("keydown", (e) => {
+    keys[e.code] = true; // Space: true
+    
+});
+
+document.addEventListener("keyup", (e) => {
+    keys[e.code] = false; // Space: false
+})
+
+//herni smycka
+const gameLoop = () => {
+    console.log(keys);
+    //resizeCanvas
+    resizeCanvas();
+
+    //clearCanvas
+    clearCanvas();
+
+    //update
+    update();
+
+    //render
+    render();
+
+    //fps
+    getFps();
+
+    window.requestAnimationFrame(gameLoop);
+}
+
+const resizeCanvas = () => {
+    canvas.width = 1280;
+    canvas.height = 720;
+}
+const clearCanvas = () => {
+    //premalujeme cele platno pozadim hry
+    ctx.drawImage(background, 0, 0, 1280, 720);
+}
+const update = () => {}
+const render = () => {}
+const getFps = () => {}
+
+window.onload = () => {
+    //jakmile se stranka nacte, vazadame si prvni snimek herni smycky
+    window.requestAnimationFrame(gameLoop);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
